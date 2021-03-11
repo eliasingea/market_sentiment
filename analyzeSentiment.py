b28014dc472b027c199487edf197f9514b49df8f
Elias Ingea <eliasingea@eliass-mbp.myfiosgateway.com>
2021-03-10 19:37:56 -0500

    create new analyzer and change to mongo

---
+++ analyzeSentiment.py
@@ -0,0 +1,44 @@
+from transformers import TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer
+import urllib
+import csv
+from scipy.special import softmax
+import numpy as np
+import tensorflow as tf
+
+
+task='sentiment'
+MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model = TFAutoModelForSequenceClassification.from_pretrained(MODEL)
+
+def create_labels():
+  labels=[]
+  mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/sentiment/mapping.txt"
+  with urllib.request.urlopen(mapping_link) as f:
+      html = f.read().decode('utf-8').split("\n")
+      csvreader = csv.reader(html, delimiter='\t')
+  labels = [row[1] for row in csvreader if len(row) > 1]
+  return labels
+
+def returnSentiment(text):
+  encoded_input = tokenizer(text, return_tensors='tf')
+  print("encoded_input")
+  output = model(encoded_input)
+  scores = output[0][0].numpy()
+  scores = softmax(scores)
+
+  ranking = np.argsort(scores)
+  ranking = ranking[::-1]
+  return_var = {}
+  labels = create_labels()
+  for i in range(scores.shape[0]):
+      l = labels[ranking[i]]
+      s = scores[ranking[i]]
+      return_var[l] = np.round(float(s), 4)
+
+  return return_var
+
+
+text = "This is a positive tweet :)"
+returnSentiment(text)
---
+++ twitter_stream.py
@@ -0,0 +1,57 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+import tweepy
+from tweepy.streaming import StreamListener
+import datetime
+from unidecode import unidecode
+import time
+import pandas as pd
+import json
+import pymongo
+from dotenv import load_dotenv
+import os
+from analyzeSentiment import returnSentiment
+
+load_dotenv("env/keys.env")
+
+client = pymongo.MongoClient("localhost", 27017)
+db = client.sentiment
+twitter_collection = db.twitter
+
+
+# Insert your twitter API key here 
+ckey=os.getenv('TWITTER_KEY')
+csecret=os.getenv('TWITTER_SECRET')
+atoken=os.getenv('TWITTER_ACCESS_TOKEN')
+asecret=os.getenv('TWITTER_TOKEN_SECRET')
+
+
+class listener(StreamListener):
+    def on_status(self,data):
+        try: 
+            text = data.extended_tweet["full_text"] 
+        except AttributeError:
+            text = data.text
+        created_at = data.created_at
+        author = str(data.author.screen_name)
+        vs = returnSentiment(unidecode(text))
+        sentiment = vs
+        twitter_collection.insert({
+            "date_time": created_at, 
+            "author": author, 
+            "tweet": text, 
+            "sentiment": sentiment
+        })       
+    def on_error(self,status):
+        print(status)
+
+## Connecting to twitter and establishing a live stream 
+while True:
+    try:
+        auth = OAuthHandler(ckey, csecret)
+        auth.set_access_token(atoken, asecret)
+        twitterStream = Stream(auth, listener(), tweet_mode='extended')
+        twitterStream.filter(track=["$AAPL", "$TSLA", "$GME"]) #this tracks any tweet with a $ symbol. Unlike Reddit, a large proportion of twitter users use $ before the stock tickers
+    except Exception as e:
+        print(str(e))
+        time.sleep(10)
